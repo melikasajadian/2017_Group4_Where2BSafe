e0a81fdda5ff2abfe2474a92bf2162e7ba970a99
melikasajadian <33864426+melikasajadian@users.noreply.github.com>
2017-12-15 10:49:04 +0100

    Tab Update

--- SpatialDecision/resources.py
+++ SpatialDecision/resources.py
@@ -2,7 +2,8 @@
 
 # Resource object code
 #
-# Created by: The Resource Compiler for PyQt4 (Qt v4.8.6)
+# Created: Fri 15. Dec 10:06:38 2017
+#      by: The Resource Compiler for PyQt (Qt v4.8.5)
 #
 # WARNING! All changes made in this file will be lost!
 
